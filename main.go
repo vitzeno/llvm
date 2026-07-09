d6a75ccd2202fab95d256a467bc6a63afb808b11
vitzeno <vitzeno@gmail.com>
2026-07-09 20:49:00 +0100

    Wire the compiler pipeline into the CLI
    main now runs parse, type-check, generate in order and gains
    -emit-llvm and -o flags for writing LLVM IR, plus -print-ast for the
    old AST debug output. Semantic errors are printed as diagnostics and
    stop compilation before code generation.

--- main.go
+++ main.go
@@ -5,44 +5,75 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/vitzeno/llvm-test/codegen"
 	"github.com/vitzeno/llvm-test/parser"
 )
 
 func main() {
 	sourceFile := flag.String("in", "", "input file")
+	emitLLVM := flag.Bool("emit-llvm", false, "emit LLVM IR")
+	output := flag.String("o", "", "output file for LLVM IR (defaults to stdout)")
+	printAST := flag.Bool("print-ast", false, "print the AST")
 	flag.Parse()
 
-	if !isFlagPassed("in") {
+	if *sourceFile == "" {
 		fmt.Println("input file is required")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	parser.YYDebug = 0
 	file, err := os.Open(*sourceFile)
 	if err != nil {
-		panic(err)
+		fmt.Printf("cannot open %s: %v\n", *sourceFile, err)
+		os.Exit(1)
 	}
+	defer file.Close()
 
+	parser.YYDebug = 0
 	lexer := parser.NewLexer(file)
 	errCount := parser.YYParse(lexer)
-	if errCount != 0 {
-		fmt.Println("parsing failed found error(s) in source file")
+	for _, diag := range lexer.Errors() {
+		fmt.Println(diag.String())
+	}
+	if errCount != 0 || len(lexer.Errors()) > 0 {
+		fmt.Println("parsing failed, found error(s) in source file")
 		os.Exit(1)
 	}
 
-	for _, lexErr := range lexer.Errors() {
-		fmt.Println(lexErr.String())
+	root := lexer.Root()
+	if *printAST {
+		parser.PrintAST(root, 0)
 	}
-}
 
-func isFlagPassed(name string) bool {
-	found := false
-	flag.Visit(func(f *flag.Flag) {
-		if f.Name == name {
-			found = true
+	checker := parser.NewChecker()
+	if diags := checker.Check(root); len(diags) > 0 {
+		for i := range diags {
+			fmt.Println(diags[i].String())
 		}
-	})
+		os.Exit(1)
+	}
 
-	return found
+	if !*emitLLVM {
+		return
+	}
+
+	module, err := codegen.New().Generate(root)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	out := os.Stdout
+	if *output != "" {
+		out, err = os.Create(*output)
+		if err != nil {
+			fmt.Printf("cannot create %s: %v\n", *output, err)
+			os.Exit(1)
+		}
+		defer out.Close()
+	}
+	if _, err := module.WriteTo(out); err != nil {
+		fmt.Printf("cannot write module: %v\n", err)
+		os.Exit(1)
+	}
 }
